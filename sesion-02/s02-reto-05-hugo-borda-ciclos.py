dfd5316c71e8d294b8c092d5f68ac3196242ddbf
hugofborda <hugo.borda@gmail.com>
2021-11-02 07:43:15 -0500

    Serie Fibonacci

---
+++ sesion-02/s02-reto-05-hugo-borda-ciclos.py
@@ -0,0 +1,16 @@
+fibonacci :int = 0
+semilla :int = 1
+
+
+while fibonacci < 1000 :
+    print(fibonacci)
+    fibonacci_nuevo : int = fibonacci + semilla
+    fibonacci = semilla
+    semilla = fibonacci_nuevo
+    
+
+
+
+
+
+
